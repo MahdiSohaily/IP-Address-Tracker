315f6366d6cbd47338b71550f05538662ea3e36b
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-10-01 22:55:57 +0430

    Add form HTML code

--- src/index.js
+++ src/index.js
@@ -4,10 +4,6 @@ import './styles.css';
 function component() {
   const element = document.createElement('div');
 
-  // Lodash, currently included via a script, is required for this line to work
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
   return element;
 }
 
