3a59ddbcd6a2f97f8554c74eb6489eb9a5f3b75a
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-10-01 21:52:00 +0430

    Add style file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import _ from 'lodash';
+import './styles.css';
 
- function component() {
-   const element = document.createElement('div');
+function component() {
+  const element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
   // Lodash, now imported by this script
-   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-   return element;
- }
+  return element;
+}
 
- document.body.appendChild(component());
+document.body.appendChild(component());
