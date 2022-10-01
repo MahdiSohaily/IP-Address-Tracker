8e20cb8d438e273013cdcaf23fcee8535dc328b2
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-10-01 21:31:12 +0430

    Update webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
    output: {
      filename: '[name].bundle.js',
      path: path.resolve(__dirname, 'dist'),
+     assetModuleFilename: 'images/[name][ext]',
      clean: true,
    },
  };
