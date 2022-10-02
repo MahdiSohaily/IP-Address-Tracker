eeff8373aaa632e6bda87d371e275a0c681c455d
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-10-02 08:52:57 +0430

    Add function to get location on map

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import _ from 'lodash';
 import './styles.css';
 
-function component() {
-  const element = document.createElement('div');
-
-  return element;
+function myMap() {
+  var mapProp = {
+    center: new google.maps.LatLng(51.508742, -0.12085),
+    zoom: 5,
+  };
+  var map = new google.maps.Map(document.getElementById('googleMap'), mapProp);
 }
-
-document.body.appendChild(component());
