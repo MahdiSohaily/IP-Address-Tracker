e02e4825192d15caaa2fa05bc55a0d1c810e52ea
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-10-02 21:21:18 +0430

    Update getting data function

--- src/index.js
+++ src/index.js
@@ -5,29 +5,42 @@ const data = async (request) => {
   const response = await fetch(
     `https://geo.ipify.org/api/v2/country,city?apiKey=at_mZXwLKLEkMRCIAtxqlnRt3rO8k2if&ipAddress=${request}`
   );
+
+  if (response.status !== 200) {
+    throw new Error('Input correct IPv4 or IPv6 address.');
+  }
+
   const data = await response.json();
+  console.log(data);
   return data;
 };
-data('').then((data) => {
-  const ip = document.querySelector('.ip');
-  const location = document.querySelector('.location');
-  const timeZone = document.querySelector('.timeZone');
-  const isp = document.querySelector('.isp');
 
-  ip.innerHTML = data.ip;
-  location.innerHTML =
-    data.location.city +
-    ',' +
-    data.location.country +
-    ', <br>' +
-    data.location.geonameId;
-  timeZone.innerHTML = 'UTC' + data.location.timezone;
-  isp.innerHTML = data.isp;
-  displayMap(data.location.lat, data.location.lng);
-});
+data('')
+  .then((data) => {
+    const ip = document.querySelector('.ip');
+    const location = document.querySelector('.location');
+    const timeZone = document.querySelector('.timeZone');
+    const isp = document.querySelector('.isp');
+
+    ip.innerHTML = data.ip;
+    location.innerHTML =
+      data.location.city +
+      ',' +
+      data.location.country +
+      ', <br>' +
+      data.location.geonameId;
+    timeZone.innerHTML = 'UTC' + data.location.timezone;
+    isp.innerHTML = data.isp;
+    displayMap(data.location.lat, data.location.lng);
+  })
+  .catch((error) => displayError(error));
+
+function displayError(e) {
+  console.log(e);
+}
 
 function displayMap(lat, lng) {
-  var map = L.map('map').setView([lat, lng], 13);
+  var map = L.map('map').setView([lat, lng], 14);
   const tileUrl =
     'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw';
 
@@ -41,8 +54,8 @@ function displayMap(lat, lng) {
 
   const locationIcon = L.icon({
     iconUrl: './images/icon-location.svg',
-    iconSize: [35, 35],
+    iconSize: [47, 60],
     iconAnchor: [15, 15],
   });
-  const marker = L.marker([0, 0], { icon: locationIcon }).addTo(map);
+  const marker = L.marker([lat, lng], { icon: locationIcon }).addTo(map);
 }
