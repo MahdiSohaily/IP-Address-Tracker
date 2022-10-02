55172df5007115b9e61c41cf3bc8770a24f5cce6
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-10-02 20:43:27 +0430

    Add get map location data

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { concat } from 'lodash';
 import './styles.css';
 
 const data = async (request) => {
@@ -7,12 +8,41 @@ const data = async (request) => {
   const data = await response.json();
   return data;
 };
-var map = L.map('map').setView([37.40599, -122.078514], 13);
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 19,
-}).addTo(map);
+data('').then((data) => {
+  const ip = document.querySelector('.ip');
+  const location = document.querySelector('.location');
+  const timeZone = document.querySelector('.timeZone');
+  const isp = document.querySelector('.isp');
 
-data('192.212.174.101').then((data) => {
-  
-  console.log(data);
+  ip.innerHTML = data.ip;
+  location.innerHTML =
+    data.location.city +
+    ',' +
+    data.location.country +
+    ', <br>' +
+    data.location.geonameId;
+  timeZone.innerHTML = 'UTC' + data.location.timezone;
+  isp.innerHTML = data.isp;
+  displayMap(data.location.lat, data.location.lng);
 });
+
+function displayMap(lat, lng) {
+  var map = L.map('map').setView([lat, lng], 13);
+  const tileUrl =
+    'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw';
+
+  L.tileLayer(tileUrl, {
+    maxZoom: 18,
+    attribution: false,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+  }).addTo(map);
+
+  const locationIcon = L.icon({
+    iconUrl: './images/icon-location.svg',
+    iconSize: [35, 35],
+    iconAnchor: [15, 15],
+  });
+  const marker = L.marker([0, 0], { icon: locationIcon }).addTo(map);
+}
