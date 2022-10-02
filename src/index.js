d0a38bbd144106d6801b8aba29c4be46c52ee8fc
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-10-02 19:19:28 +0430

    Add map loader to the project

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import './styles.css';
 
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([37.40599, -122.078514], 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
-  attribution:
-    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
