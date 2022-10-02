b6e9569d4cc65794790fc02c53d7f03878c1c237
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-10-02 19:59:38 +0430

    Add function to bind data to view

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ const data = async (request) => {
     `https://geo.ipify.org/api/v2/country,city?apiKey=at_mZXwLKLEkMRCIAtxqlnRt3rO8k2if&ipAddress=${request}`
   );
   const data = await response.json();
-  console.log(data);
+  return data;
 };
 var map = L.map('map').setView([37.40599, -122.078514], 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
 }).addTo(map);
 
-data('8.8.8.8');
+data('192.212.174.101').then((data) => {
+  
+  console.log(data);
+});
