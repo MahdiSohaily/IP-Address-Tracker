7046ec78f835bc12a53d3165f5ed0b514421389f
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-10-02 19:28:22 +0430

    Add method to get user IP address data

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import './styles.css';
 
+const data = async (request) => {
+  const response = await fetch(
+    `https://geo.ipify.org/api/v2/country,city?apiKey=at_mZXwLKLEkMRCIAtxqlnRt3rO8k2if&ipAddress=`
+  );
+  const data = await response.json();
+  console.log(data);
+};
 var map = L.map('map').setView([37.40599, -122.078514], 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
 }).addTo(map);
+
+data();
